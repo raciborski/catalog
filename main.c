beae88a80b13b94e35868a037802edadb03c0de0
R. Richard <raciborski@protonmail.ch>
2020-08-06 00:01:26 +0200

    Add error checks

--- main.c
+++ main.c
@@ -10,23 +10,30 @@
 #include "node.h"
 #include "visitor.h"
 
+static const char *DB_PATH = "/home/keccak/Documents/catalog.db";
+
+static void print_error(const char *message) {
+  printf("[%serr%s] %s\n", RED, RESET, message);
+}
+
 int main(void) {
   sqlite3 *db;
   node_ops_t ops;
 
-  if(sqlite3_open("test.db", &db)) {
-    printf("Cannot open database.\n");
-    return 1;
+  if(!sqlite3_open(DB_PATH, &db)) {
+    node_ops_init(&ops, db);
+    node_ops_mark_branches(&ops, STATUS_DEL);
+    if(node_ops_select_root(&ops, visit_root_path)) {
+      node_ops_select_changes(&ops, visit_path_with_status);
+      node_ops_delete_marked(&ops);
+      node_ops_mark_branches(&ops, STATUS_NORM);
+    }
+    else
+      print_error("Failed to acccess one or multiple folders.");
+    node_ops_dest(&ops);
   }
-
-  node_ops_init(&ops, db);
-  node_ops_mark_branches(&ops, STATUS_DEL);
-  if(node_ops_select_root(&ops, visit_root_path)) {
-    node_ops_select_changes(&ops, visit_path_with_status);
-    node_ops_delete_marked(&ops);
-    node_ops_mark_branches(&ops, STATUS_NORM);
-  }
-  node_ops_dest(&ops);
+  else
+    print_error("Cannot open database.");
 
   sqlite3_close(db);
   return 0;
