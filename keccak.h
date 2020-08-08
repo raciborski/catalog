700aefc16876d18a82cadaac850741aae095b51c
R. Richard <raciborski@protonmail.ch>
2020-08-08 15:21:32 +0200

    Add include guard

--- keccak.h
+++ keccak.h
@@ -3,6 +3,11 @@
  * written by: raciborski *
  **************************/
 
+#ifndef KECCAK_H
+#define KECCAK_H
+
 #include <stdint.h>
 
 void sha3_file(uint8_t *hash, int dir_fd, const char *name);
+
+#endif
